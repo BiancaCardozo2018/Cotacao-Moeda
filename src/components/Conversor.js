5e2b86258718cd929abe373ecc5ed82c020f26d5
Bianca Lira <bianca1999@gmail.com>
2021-06-23 00:14:26 -0300

    Adicionando retorno da API fixa no item de data e cotação diária

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -9,9 +9,36 @@ export default class Conversor extends Component {
         this.state = {
             moedaA_valor: "",
             moedaB_valor:0,
+            date:0,
+            cotacaoDiaria: 0,
         }
 
         this.converter = this.converter.bind(this);
+    }
+
+    componentDidMount() {
+        this.cotacaoDiaria();
+    }
+
+    componentWillUnmount() {
+        this.converter();
+    }
+
+    cotacaoDiaria(){
+        let de_para = `${this.props.moedaA}-${this.props.moedaB}`;
+        let url = `https://economia.awesomeapi.com.br/${de_para}`;
+
+        fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(json=>{
+            let cotacao = json[0]['high'];
+            let date = json[0]['create_date'];
+            let cotacaoDiaria = ( parseFloat(1) * cotacao).toFixed(2);
+            this.setState({date});
+            this.setState({cotacaoDiaria});
+        })
     }
 
     converter(){
@@ -26,11 +53,7 @@ export default class Conversor extends Component {
         .then(json=>{
             let cotacao = json[0]['high'];
             let moedaB_valor = ( parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
-            let date = json[0]['create_date'];
-            let cotacaoDiaria = ( parseFloat(1) * cotacao).toFixed(2);
             this.setState({moedaB_valor});
-            this.setState({date});
-            this.setState({cotacaoDiaria});
         })
        
     }
