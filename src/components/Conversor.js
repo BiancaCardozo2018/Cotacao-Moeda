2d736f68ddbe2a6932a8407f89cbd41c052527bd
Bianca Lira <bianca1999@gmail.com>
2021-06-23 17:17:58 -0300

    Criando o cálculo de conversão em dinheiro

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -13,7 +13,8 @@ export default class Conversor extends Component {
         this.state = {
             moedaA_valor: "",
             moedaB_valor:0,
-            completDate: date
+            completDate: date,
+            taxaEstado_valor: ""
         }
 
         this.converter = this.converter.bind(this);
@@ -40,6 +41,8 @@ export default class Conversor extends Component {
             let cotacao = json[0]['high'];
             let cotacaoDiaria = ( parseFloat(1) * cotacao).toFixed(2);
             this.setState({cotacaoDiaria});
+
+
         })
     }
 
@@ -54,9 +57,23 @@ export default class Conversor extends Component {
         })
         .then(json=>{
             let cotacao = json[0]['high'];
-            let moedaB_valor = ( parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
+            let cotacao_round = (parseFloat(1) * cotacao).toFixed(2);
+            cotacao_round = parseFloat(cotacao_round)
+            
+            let percent_taxaEstado = parseFloat(this.state.taxaEstado_valor)/100;
+            let percent_taxaDinheiro = 1.10/100;
+            
+            let moedaA_percent_taxaEstado = percent_taxaEstado * parseFloat(this.state.moedaA_valor)
+            let cotacao_round_percent_taxaDinheiro = percent_taxaDinheiro * cotacao_round
+            let moedaA_sum_percent_taxaEstado = moedaA_percent_taxaEstado + parseFloat(this.state.moedaA_valor)
+            let cotacao_round_sum_percent_taxaDinheiro = cotacao_round + cotacao_round_percent_taxaDinheiro
+
+            let moedaB_valor = (moedaA_sum_percent_taxaEstado * cotacao_round_sum_percent_taxaDinheiro).toFixed(2);
+
             this.setState({moedaB_valor});
+
         })
+        
        
     }
    
@@ -66,6 +83,12 @@ export default class Conversor extends Component {
                 <h2> {this.state.completDate} | R$ {this.state.cotacaoDiaria}</h2>
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
                 <input type="text" onChange={(event)=>(this.setState({moedaA_valor:event.target.value}))}></input>
+                <input type="text" onChange={(event)=>(this.setState({taxaEstado_valor:event.target.value}))}></input>
+                <h2>Tipo de Compra</h2>
+                <div>
+                    <p>Dinheiro</p><input type="radio" name="forma_pagamento" value="cartao"/>
+                    <p>Cartão</p><input type="radio" name="forma_pagamento" value="dinheiro"/>
+                </div>
                 <input type="button" id="btn_converter" value="Converter" onClick = {this.converter}></input>
                 <h2>{this.state.moedaB_valor}</h2>
             </div>
